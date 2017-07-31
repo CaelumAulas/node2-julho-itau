e6e8fc9c056a0949226ad04e15e98e9907b77640
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-07-31 14:58:53 -0300

    Até o exercício 1.6 - Criaando a primeira rota do projeto

---
+++ helloWorld.js
@@ -0,0 +1,20 @@
+let nome = 'Marco'
+const nomes = ['Marco', 'Ceci', 'Jovis', 'Guai']
+const idade = calcularMinhaIdade(1988)
+
+nome = 'Marco Bruno'
+
+for(let index = 0; index < nomes.length; index++) {
+  let coiso = 'djkhafkjhf'
+  coiso = 'Coiso'
+  console.log(nomes[index])
+}
+
+function calcularMinhaIdade(anoDeNascimento) {
+  var anoAtual = 2017
+
+  return anoAtual - anoDeNascimento
+}
+// console.log('Variável ' + anoAtual)
+
+console.log(`Olá ${nome} com tal idade ${calcularMinhaIdade(1987)}`)
