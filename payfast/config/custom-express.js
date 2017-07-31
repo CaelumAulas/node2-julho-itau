8645d099becfe5693da4a5d796c2bc3ac4a133e3
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-07-31 14:58:39 -0300

    Até o exercício 1.6 - Criaando a primeira rota do projeto

---
+++ payfast/config/custom-express.js
@@ -0,0 +1,27 @@
+module.exports = function() {
+  const express = require('express')
+  const load = require('express-load')
+  const app = express()
+
+  // const controllerPgamentos = require('../controllers/pagamentos')
+  // controllerPgamentos(app)
+  // require('../controllers/pagamentos')(app)
+  // require('../controllers/home')(app)
+
+  load('controllers').into(app)
+
+  return app
+}
+
+const express = require('express')
+const load = require('express-load')
+const app = express()
+
+// const controllerPgamentos = require('../controllers/pagamentos')
+// controllerPgamentos(app)
+// require('../controllers/pagamentos')(app)
+// require('../controllers/home')(app)
+
+load('controllers').into(app)
+
+module.exports = app 
