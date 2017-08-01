a7eed7c8e8b66c3cc4e3169b159dde3aee662479
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-08-01 16:08:18 -0300

    Um middleware simples implementado

--- payfast/index.js
+++
@@ -1,8 +0,0 @@
-//import app from './config/custom-express'
-const app = require('./config/custom-express')()
-const port = 3000
-
-app.listen(port, function() {
-  console.log(`Servidor de pé em http://localhost:${port}`)
-  console.log('Para derrubar o servidor: ctrl+c')
-})
